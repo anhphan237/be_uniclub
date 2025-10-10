09ba6ae1d3f120a2471223de5d5985e7a490463c
Pham Trung Nguyen <nguyenn30103@gmail.com>
2025-10-11 00:22:33 +0700

    delete staff in User.java

--- src/main/java/com/example/uniclub/entity/User.java
+++ src/main/java/com/example/uniclub/entity/User.java
@@ -29,8 +29,6 @@ public class User {
     @Column(nullable = false)
     private String status = UserStatusEnum.ACTIVE.name();
 
-    @Column(nullable = false)
-    private boolean staff = false;
 
     @ManyToOne(optional = false)
     @JoinColumn(name = "role_id")
