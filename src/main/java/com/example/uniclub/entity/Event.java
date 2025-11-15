3df532a7ceebb8a6c941afb1536ab855a220b2cc
Pham Trung Nguyen <nguyenn30103@gmail.com>
2025-11-16 06:24:34 +0700

    Update event feature + DB change

--- src/main/java/com/example/uniclub/entity/Event.java
+++ src/main/java/com/example/uniclub/entity/Event.java
@@ -36,6 +36,7 @@ public class Event {
     @Column(nullable = false)
     private String name;
 
+    @Column(columnDefinition = "TEXT")
     private String description;
 
     @Enumerated(EnumType.STRING)
